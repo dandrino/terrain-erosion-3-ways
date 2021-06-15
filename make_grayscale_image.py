d02201340d6e68cd3a195a44c3452aceb2634b0d
MightyBOBcnc <mightybob2000@gmail.com>
2021-06-15 00:46:06 -0400

    Formatting cleanup

--- make_grayscale_image.py
+++ make_grayscale_image.py
@@ -9,14 +9,17 @@ import util
 
 
 def main(argv):
-  parser = argparse.ArgumentParser(description="Generates a PNG containing the terrain height in grayscale.")
-  parser.add_argument("input_array", help="<input_array.np[yz]> (include file extension)")
-  parser.add_argument("output_image", help="<output_image.png> (include file extension)")
+  parser = argparse.ArgumentParser(
+    description="Generates a PNG containing the terrain height in grayscale.")
+  parser.add_argument("input_array", 
+    help="<input_array.np[yz]> (include file extension)")
+  parser.add_argument("output_image", 
+    help="<output_image.png> (include file extension)")
   args = parser.parse_args()
 
   my_dir = os.path.dirname(argv[0])
   output_dir = os.path.join(my_dir, 'output')
-  
+
   input_path = args.input_array
   output_path = os.path.join(output_dir, args.output_image)
 
